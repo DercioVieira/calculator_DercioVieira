3b3aed4955bd52375775f40922154e7f8d6cee68
Dércio Vieira <yends2222@gmail.com>
2024-05-23 19:59:47 -0100

    feature_multiplicacao

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Python program for simple calculator
-
-# Function to add two numbers
-def add(num1, num2):
-	return num1 + num2
+# Function to multiply two numbers
+def multiply(num1, num2):
+	return num1 * num
